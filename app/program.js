7022dd74339f1bd7a22e8e8d7ef0d66bcd0a87c5
Michał Kuliś <michalkulis1993@gmail.com>
2018-07-04 23:07:06 +0200

    Add app/ folder with program.js file

---
+++ app/program.js
@@ -0,0 +1,29 @@
+var OSinfo = require('../modules/OSinfo');
+
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function () {
+    var input = process.stdin.read();
+    if (input !== null) {
+        
+        var instruction = input.trim();
+        
+        switch (instruction) {
+            case '/exit':
+                process.stdout.write('Quiting app!\n');
+                process.exit();
+                break;
+            case '/version':
+                console.log(process.versions);
+                break;
+            case '/language':
+                console.log(process.env);
+                break;
+            case '/getOSinfo':
+                OSinfo.print();
+                break;
+            default:
+                process.stdout.write('Wrong instruction!\n');
+        }
+    }
+});
