4c2872dc82553a2c6fdc9ae4f61eb8a67e76b61c
Michał Kuliś <michalkulis1993@gmail.com>
2018-07-06 01:40:52 +0200

    Update timeConverter.js

--- modules/timeConverter.js
+++ modules/timeConverter.js
@@ -1,9 +1,10 @@
 function toMinute(Uptime) {
-    var minute = (Uptime/60).toFixed(0);
+    var minute = (Uptime / 60).toFixed(0);
     return minute;
 };
+
 function toHours(Uptime) {
-    var hours = ((Uptime/60)/60).toFixed(0);
+    var hours = ((Uptime / 60) / 60).toFixed(0);
     return hours;
 };
 
